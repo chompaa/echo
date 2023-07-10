2bd254d5d0c7161470f987bdb3cf6cb357af7929
chompaa <antony.m.3012@gmail.com>
2023-07-10 18:05:51 +0100

    feat: store cart in local storage

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { ProductData } from "../data/data";
 
 interface Cart {
@@ -13,40 +14,49 @@ interface Cart {
   decrementQuantity: (id: number) => void;
 }
 
-export const useCart = create<Cart>((set, get) => ({
-  cart: [],
-  cartVisible: false,
-  addToCart: (product: ProductData) =>
-    set((state) => ({ cart: [...state.cart, { ...product, quantity: 1 }] })),
-  removeFromCart: (id: number) =>
-    set((state) => ({
-      cart: state.cart.filter((product) => product.id !== id),
-    })),
-  toggleCartVisible: () =>
-    set((state) => ({ cartVisible: !state.cartVisible })),
-  getTotal: () =>
-    get()
-      .cart.reduce(
-        (total, product) => total + product.price * product.quantity,
-        0
-      )
-      .toFixed(2),
-  isInCart: (id: number) =>
-    get().cart.some((product: ProductData) => product.id === id),
-  incrementQuantity: (id: number) =>
-    set((state) => ({
-      cart: state.cart.map((product) =>
-        product.id === id
-          ? { ...product, quantity: product.quantity + 1 }
-          : product
-      ),
-    })),
-  decrementQuantity: (id: number) =>
-    set((state) => ({
-      cart: state.cart.map((product) =>
-        product.id === id && product.quantity > 1
-          ? { ...product, quantity: product.quantity - 1 }
-          : product
-      ),
-    })),
-}));
+export const useCart = create(
+  persist<Cart>(
+    (set, get) => ({
+      cart: [],
+      cartVisible: false,
+      addToCart: (product: ProductData) =>
+        set((state) => ({
+          cart: [...state.cart, { ...product, quantity: 1 }],
+        })),
+      removeFromCart: (id: number) =>
+        set((state) => ({
+          cart: state.cart.filter((product) => product.id !== id),
+        })),
+      toggleCartVisible: () =>
+        set((state) => ({ cartVisible: !state.cartVisible })),
+      getTotal: () =>
+        get()
+          .cart.reduce(
+            (total, product) => total + product.price * product.quantity,
+            0
+          )
+          .toFixed(2),
+      isInCart: (id: number) =>
+        get().cart.some((product: ProductData) => product.id === id),
+      incrementQuantity: (id: number) =>
+        set((state) => ({
+          cart: state.cart.map((product) =>
+            product.id === id
+              ? { ...product, quantity: product.quantity + 1 }
+              : product
+          ),
+        })),
+      decrementQuantity: (id: number) =>
+        set((state) => ({
+          cart: state.cart.map((product) =>
+            product.id === id && product.quantity > 1
+              ? { ...product, quantity: product.quantity - 1 }
+              : product
+          ),
+        })),
+    }),
+    {
+      name: "cart",
+    }
+  )
+);
